ec9548d14670802f7e2a0ea9c405049cc8651c3b
excitingmvr <excitingmvr@gmail.com>
2025-02-27 15:42:34 +0900

    first

---
+++ innova/src/main/java/com/a2a2lab/InnovaApplication.java
@@ -0,0 +1,13 @@
+package com.a2a2lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnovaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InnovaApplication.class, args);
+	}
+
+}
